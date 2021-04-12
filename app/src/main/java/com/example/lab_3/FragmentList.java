7708cca460d2e3b47d126824c72c2f267f9c044f
waffillka <waffilka@gmail.com>
2021-04-12 20:55:30 +0300

    Add FragmentList

--- app/src/main/java/com/example/lab_3/FragmentList.java
+++ app/src/main/java/com/example/lab_3/FragmentList.java
@@ -1,53 +1,72 @@
 package com.example.lab_3;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.lab_3.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FragmentList extends Fragment implements View.OnClickListener {
+
+    Button buttonAdd;
+    private ArrayList<Uri> images;
+    private ArrayList<Uri> songs;
+    private static ListAdapter adapter;
+    private ListView list;
 
 
-public class FragmentList extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+        View rootView = inflater.inflate(R.layout.fragment_list, container, false);
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        buttonAdd = rootView.findViewById(R.id.buttonAdd);
+        list = rootView.findViewById(R.id.my_list);
+        buttonAdd.setOnClickListener(this);
 
-    public FragmentList() {
-        // Required empty public constructor
-    }
+        images = new ArrayList<>();
+        songs = new ArrayList<>();
+        adapter = new ListAdapter(getActivity().getApplicationContext(), images, songs);
+        list.setAdapter(adapter);
+        list.setScrollingCacheEnabled(false);
 
-
-    // TODO: Rename and change types and number of parameters
-    public static FragmentList newInstance(String param1, String param2) {
-        FragmentList fragment = new FragmentList();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return rootView;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onClick(View view) {
+        OnAddButtonListener listener = (OnAddButtonListener) getActivity();
+        listener.onAddButtonClicked(buttonAdd);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+    public void setImageMusic(Uri imageUri, Uri musicUri){
+        images.add(0, imageUri);
+        songs.add(0, musicUri);
+        //Collections.reverse(images);
+        adapter.changePosition();
+        adapter.notifyDataSetChanged();
+        list.smoothScrollToPosition(0);
     }
+
+    public interface OnAddButtonListener {
+        void onAddButtonClicked(Button add);
+    }
+
 }
